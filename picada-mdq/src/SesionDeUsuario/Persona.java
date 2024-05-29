fa73677d04d1f090d76655d8a80a6103de56e0d0
ivanamaya1 <ivancitohh2004@hotmail.com>
2024-05-29 18:57:34 -0300

    Agrego metodos a Persona
    Se agrega personaToJSON y JSONToPersona, con el agregado de calle y numero se crea el jsonObject y adentro otro jsonObject con la dirección.

--- picada-mdq/src/SesionDeUsuario/Persona.java
+++ picada-mdq/src/SesionDeUsuario/Persona.java
@@ -48,7 +48,28 @@ public class Persona {
         return email;
     }
     
+    public JSONObject personaToJSON() throws Exception{
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("nombre",nombre);
+        jsonObject.put("apellido", apellido);
+        JSONObject direccion = new JSONObject();
+        direccion.put("calle", calle);
+        direccion.put("numero", numero);
+        jsonObject.put("direccion", direccion);
+        jsonObject.put("email", email);
+        return jsonObject;
+    }
 
+    public static Persona JSONToPersona(JSONObject persona) throws Exception{
+        String nombre = persona.getString("nombre");
+        String apellido = persona.getString("apellido");
+        JSONObject direccion = persona.getJSONObject("direccion");
+        String calle = direccion.getString("calle");
+        int numero = direccion.getInt("numero");
+        String email = persona.getString("email");
+        Persona persona1 = new Persona(nombre, apellido, calle, numero, email);
+        return persona1;
+    }
     @Override
     public String toString() {//Metodo toString para mostrar los datos de la clase persona
         return "Persona{" +
